0225f86aec9a524ca43a5cd014f2349bd5ed74d6
Masud Rana <masud040@gmail.com>
2023-12-31 00:28:29 +0600

    add edit modal

--- src/components/Card/ManageMenuCard.jsx
+++ src/components/Card/ManageMenuCard.jsx
@@ -1,24 +1,51 @@
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import MenuEditModal from "../Modal/MenuEditModal";
+import { useState } from "react";
 const ManageMenuCard = ({ item }) => {
   const { name, price, description, image_url } = item || {};
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
   return (
-    <div className="flex justify-between items-center text-dark-gray border gap-3 border-gray-300 rounded-lg p-2 group ">
-      <div className="space-y-1 flex-1    ">
-        <h2 className="text-lg text-gray-800 font-semibold">{name}</h2>
-        <p className="text-gray-600 ">{description}</p>
-        <h4 className="text-[18px] text-gray-800 font-semibold">
-          Price: {price}
-        </h4>
-      </div>
-      <div className="relative">
-        <img
-          src={image_url}
-          className="rounded-lg mx-auto h-[120px] group-hover:scale-110 
+    <>
+      <div className="flex justify-between items-center text-dark-gray border gap-3 border-gray-300 rounded-lg p-2 group ">
+        <div className="space-y-1 flex-1    ">
+          <h2 className="text-lg text-gray-800 font-semibold">{name}</h2>
+          <p className="text-gray-600 ">{description}</p>
+
+          <h4 className="text-[18px] text-gray-800 font-semibold">
+            Price: {price}
+          </h4>
+        </div>
+        <div className="relative">
+          <img
+            src={image_url}
+            className="rounded-lg mx-auto h-[120px] group-hover:scale-110 
       transition w-[120px]"
-          alt=""
-        />
-        <div className="absolute bottom-1 right-1 text-2xl flex gap-2"></div>
+            alt=""
+          />
+          <div className="absolute bottom-1 right-1 text-3xl flex gap-4">
+            <button
+              onClick={openModal}
+              className="  transition-all delay-100    rounded-full text-pink-400"
+            >
+              <FaEdit />
+            </button>
+            <button className="  transition-all delay-100     rounded-full text-red-800">
+              <MdDelete />
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+      <MenuEditModal isOpen={isOpen} closeModal={closeModal} item={item} />
+    </>
   );
 };
 
--- src/pages/Seller/ManageMenu.jsx
+++ src/pages/Seller/ManageMenu.jsx
@@ -32,7 +32,7 @@ const ManageMenu = () => {
           </button>
         </div>
       </div>
-      <div className="my-8 grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="my-8 grid grid-cols-1 md:grid-cols-2 gap-6">
         {!isLoading &&
           menuItems?.map((item) => (
             <ManageMenuCard key={item._id} item={item} />
