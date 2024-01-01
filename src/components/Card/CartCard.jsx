d6fb317df6d34859c6858cb584b82bf3bfe72ef9
Masud Rana <masud040@gmail.com>
2024-01-01 12:30:14 +0600

    add funtionality for cart item count increment and decrement

--- src/components/Card/CartCard.jsx
+++ src/components/Card/CartCard.jsx
@@ -1,12 +1,23 @@
 import { FaRegHeart } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import { IoIosRemove } from "react-icons/io";
-import { IoMdAdd } from "react-icons/io";
+import { IoIosRemove, IoMdAdd } from "react-icons/io";
+import { useState } from "react";
+
 const CartCard = ({ order, isSelected, handleChange }) => {
   const { name, price, image, count } = order || {};
-
+  const [totalCount, setTotalCount] = useState(count);
+  const handleDecrement = () => {
+    if (totalCount > 1) {
+      setTotalCount(totalCount - 1);
+    }
+  };
+  const handleIncrement = () => {
+    if (totalCount < 5) {
+      setTotalCount(totalCount + 1);
+    }
+  };
   return (
-    <div className="flex gap-4 items-center">
+    <div className="flex gap-5 items-center">
       <input
         type="checkbox"
         value={order}
@@ -19,10 +30,34 @@ const CartCard = ({ order, isSelected, handleChange }) => {
       <div>
         <h3 className=" font-semibold">{name}</h3>
         <h4>{price}</h4>
-        <div className="flex items-center">
-          <FaRegHeart />
-          <RiDeleteBin6Line />
-          <div></div>
+        <div className="flex justify-between gap-1 items-center">
+          <button className="p-1.5 text-lg text-primary">
+            <FaRegHeart />
+          </button>
+          <button className="p-1.5 text-lg text-red-700">
+            <RiDeleteBin6Line />
+          </button>
+          <div className="flex items-center">
+            <button
+              onClick={handleDecrement}
+              className="bg-primary p-1 rounded-l-sm"
+            >
+              <IoIosRemove />
+            </button>
+            <input
+              type="number"
+              max="5"
+              min="0"
+              value={totalCount}
+              className="w-8  border text-center rounded-sm bg-gray-200 focus:outline-none focus:border-pink-300 "
+            />
+            <button
+              onClick={handleIncrement}
+              className="bg-primary p-1 rounded-r-sm"
+            >
+              <IoMdAdd />
+            </button>
+          </div>
         </div>
       </div>
     </div>
