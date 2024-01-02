b584e507a7f8a2e8fa6801ac251eb706f62ee6b9
Masud Rana <masud040@gmail.com>
2024-01-02 23:33:55 +0600

    add address details card in address page

--- src/hooks/useGetAddress.jsx
+++ src/hooks/useGetAddress.jsx
@@ -5,15 +5,15 @@ import useAxiosSecure from "./useAxiosSecure";
 const useGetAddress = () => {
   const { user, loading } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: address, refetch } = useQuery({
+  const { data: userAddress, refetch } = useQuery({
     enabled: !loading && !!user?.email,
     queryKey: ["address", user?.email],
     queryFn: async () => {
-      const { data } = await axiosSecure.get(``);
+      const { data } = await axiosSecure.get(`/address/${user?.email}`);
       return data;
     },
   });
-  return [address, refetch];
+  return [userAddress, refetch];
 };
 
 export default useGetAddress;
--- src/pages/User/AddressBook.jsx
+++ src/pages/User/AddressBook.jsx
@@ -1,22 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
-
 import { useState } from "react";
-import useAxiosSecure from "../../hooks/useAxiosSecure";
-import useAuth from "../../hooks/useAuth";
+
 import AddressModal from "../../components/Modal/AddressModal";
 import { IoMdAdd } from "react-icons/io";
+import useGetAddress from "../../hooks/useGetAddress";
 const AddressBook = () => {
-  const { user, loading } = useAuth();
-  const axiosSecure = useAxiosSecure();
   const [isOpen, setIsOpen] = useState(false);
-  const { data: city } = useQuery({
-    enabled: !loading && !!user?.email,
-    queryKey: ["address", user?.email],
-    queryFn: async () => {
-      const { data } = await axiosSecure.get(``);
-      return data?.data;
-    },
-  });
+
+  const [userAddress, refetch] = useGetAddress();
+  const {
+    _id,
+    name,
+
+    address,
+    mobile,
+
+    division,
+    place,
+    city,
+    area,
+  } = userAddress || {};
 
   const handleShowModal = () => {
     setIsOpen(true);
@@ -27,18 +29,34 @@ const AddressBook = () => {
 
   return (
     <>
-      <div className="flex justify-end">
-        <div
-          onClick={handleShowModal}
-          className=" flex items-center justify-end gap-1 bg-gray-200 p-2 rounded-md w-max"
-        >
-          <span>
-            <IoMdAdd className="text-xl text-primary" />
-          </span>
-          <button className="text-sm  ">Add New Address</button>
+      {!userAddress && (
+        <>
+          <div className="flex justify-end">
+            <div
+              onClick={handleShowModal}
+              className=" flex items-center justify-end gap-1 bg-gray-200 p-2 rounded-md w-max mb-3"
+            >
+              <span>
+                <IoMdAdd className="text-xl text-primary" />
+              </span>
+              <button className="text-sm  ">Add Address</button>
+            </div>
+          </div>
+        </>
+      )}
+      <div className="text-sm text-gray-800 space-y-1 w-96 bg-white border p-4 rounded-md hover:drop-shadow-xl transition-all duration-500">
+        <div className="flex items-center justify-between ">
+          <p>{name}</p>
+          <button className="text-xs text-indigo-500">EDIT</button>
         </div>
+        <p>{mobile}</p>
+        <p>
+          {division},{city},{area},{address?.split(",")[0]}
+        </p>
+        <p className="text-xs bg-blue-50 text-green-600 w-max px-1 rounded-sm">
+          {place}
+        </p>
       </div>
-
       <AddressModal isOpen={isOpen} closeModal={closeModal} />
     </>
   );
