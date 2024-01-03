f55983fd77469a96704c582d1731c89d343f2ab3
Masud Rana <masud040@gmail.com>
2024-01-03 15:42:17 +0600

    add ordered prodcut section in checkout page

--- src/components/Checkout/ProductsDetails.jsx
+++ src/components/Checkout/ProductsDetails.jsx
@@ -1,4 +1,6 @@
-const ProductsDetails = () => {
+const ProductsDetails = ({ ids }) => {
+  const idArray = ids?.split(",");
+
   return <div></div>;
 };
 
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom";
 import useGetAddress from "../../hooks/useGetAddress";
 import BillingAddress from "../../components/Checkout/BillingAddress";
+import ProductsDetails from "../../components/Checkout/ProductsDetails";
 
 const Checkout = () => {
   const location = useLocation();
@@ -14,7 +15,9 @@ const Checkout = () => {
     <div className="my-8 md:grid grid-cols-3 gap-8 space-y-8 md:space-y-0">
       <div className=" md:col-span-2 text-gray-700">
         <BillingAddress />
-        <div className="border min-h-[200px] mt-8 rounded-lg drop-shadow-xl bg-white border-gray-300"></div>
+        <div className="border min-h-[200px] mt-8 rounded-lg drop-shadow-xl bg-white border-gray-300">
+          <ProductsDetails ids={ids} />
+        </div>
       </div>
       <div className="border min-h-[200px] rounded-lg md:col-span-1 drop-shadow-xl bg-white border-gray-300"></div>
     </div>
