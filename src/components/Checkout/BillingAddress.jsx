9e0ff96028fa0f92fc6f2ab5e7368e203503fb17
Masud Rana <masud040@gmail.com>
2024-01-03 15:37:43 +0600

    add billing address section in checkout page

---
+++ src/components/Checkout/BillingAddress.jsx
@@ -0,0 +1,38 @@
+import useGetAddress from "../../hooks/useGetAddress";
+
+const BillingAddress = () => {
+  const [userAddress] = useGetAddress();
+  const {
+    name,
+    email,
+    address,
+    mobile,
+
+    division,
+    place,
+  } = userAddress || {};
+  return (
+    <div className="border  rounded-lg drop-shadow-xl bg-white border-gray-300 px-4 py-6 text-xs  space-y-2">
+      <p>Deliver to: {name}</p>
+      <div className="flex gap-1.5 items-center">
+        <p className="bg-indigo-50 text-indigo-400 px-1 rounded-sm">{place}</p>
+        <p>{mobile}</p>
+        <p> | </p>
+        <p>
+          {address}, {division}
+        </p>
+        <button className="text-indigo-500">Change</button>
+      </div>
+      <div className="flex items-center gap-2">
+        <p>Bill to the same address</p>
+        <button className="text-indigo-500">Edit</button>
+      </div>
+      <div className="flex items-center gap-2">
+        <p>Email to {email}</p>
+        <button className="text-indigo-500">Edit</button>
+      </div>
+    </div>
+  );
+};
+
+export default BillingAddress;
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import useGetAddress from "../../hooks/useGetAddress";
+import BillingAddress from "../../components/Checkout/BillingAddress";
 
 const Checkout = () => {
   const location = useLocation();
@@ -8,26 +9,14 @@ const Checkout = () => {
   const total = params.get("total");
   const subTotal = params.get("subtotal");
   const discount = params.get("discount");
-  const [userAddress, refetch] = useGetAddress();
-  const {
-    name,
-    email,
-    address,
-    mobile,
-    landmark,
-    division,
-    place,
-    city,
-    area,
-  } = userAddress || {};
 
   return (
-    <div className="mt-8 md:grid grid-cols-3 gap-8 space-y-8 md:space-y-0">
-      <div className="border min-h-[200px] md:col-span-2">
-        <div></div>
-        <div></div>
+    <div className="my-8 md:grid grid-cols-3 gap-8 space-y-8 md:space-y-0">
+      <div className=" md:col-span-2 text-gray-700">
+        <BillingAddress />
+        <div className="border min-h-[200px] mt-8 rounded-lg drop-shadow-xl bg-white border-gray-300"></div>
       </div>
-      <div className="border min-h-[200px] rounded-lg md:col-span-1"></div>
+      <div className="border min-h-[200px] rounded-lg md:col-span-1 drop-shadow-xl bg-white border-gray-300"></div>
     </div>
   );
 };
