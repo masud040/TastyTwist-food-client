89c7c40517edc548240560f269ea39d5502b175e
Masud Rana <masud040@gmail.com>
2024-01-03 23:14:16 +0600

    complete pay fuctionality

--- src/components/Checkout/OrderSummary.jsx
+++ src/components/Checkout/OrderSummary.jsx
@@ -3,38 +3,37 @@ import { loadStripe } from "@stripe/stripe-js";
 import PlaceOrder from "./PlaceOrder";
 const stripePromise = loadStripe(import.meta.env.VITE_stripe_publishable_key);
 const OrderSummary = ({ subTotal, shippingCost, discount, total }) => {
-  const options = {
-    clientSecret: {},
-  };
   return (
-    <Elements stripe={stripePromise} options={options}>
-      <div className="border min-h-[150px] md:h-[280px] rounded-lg drop-shadow-xl bg-white border-gray-300 text-xs px-4 py-6 space-y-1">
-        <p className="font-semibold text-sm">Order Summary</p>
-        <div className="flex justify-between items-center">
-          <p className="font-semibold">Items Total</p>
-          <p className="text-gray-700">TK {subTotal}</p>
-        </div>
-        <div className="flex justify-between items-center">
-          <p className="font-semibold">Delivery Fee</p>
-          <p className="text-gray-700">TK {shippingCost}</p>
-        </div>
-        {discount > 0 && (
-          <div className="flex justify-between items-center">
-            <p className="font-semibold">Discount</p>
-            <p className="text-gray-700">TK {discount}</p>
-          </div>
-        )}
-        <div className="flex justify-between items-center">
-          <p className="font-semibold">Total Payment</p>
-          <p className="text-gray-700">TK {total}</p>
-        </div>
-        <p className="text-xs text-end text-gray-600">
-          VAT included, where applicable
-        </p>
-        <hr className="border border-gray-400" />
-        <PlaceOrder />
+    <div className="border min-h-[150px] md:h-[280px] rounded-lg drop-shadow-xl bg-white border-gray-300 text-xs px-4 py-6 space-y-1">
+      <p className="font-semibold text-sm">Order Summary</p>
+      <div className="flex justify-between items-center">
+        <p className="font-semibold">Items Total</p>
+        <p className="text-gray-700">TK {subTotal}</p>
       </div>
-    </Elements>
+      <div className="flex justify-between items-center">
+        <p className="font-semibold">Delivery Fee</p>
+        <p className="text-gray-700">TK {shippingCost}</p>
+      </div>
+      {discount > 0 && (
+        <div className="flex justify-between items-center">
+          <p className="font-semibold">Discount</p>
+          <p className="text-gray-700">TK {discount}</p>
+        </div>
+      )}
+      <div className="flex justify-between items-center">
+        <p className="font-semibold">Total Payment</p>
+        <p className="text-gray-700">TK {total}</p>
+      </div>
+      <p className="text-[11px] pb-1 text-end text-gray-600">
+        VAT included, where applicable
+      </p>
+      <hr className="border" />
+      <div className="pt-2">
+        <Elements stripe={stripePromise}>
+          <PlaceOrder total={total} />
+        </Elements>
+      </div>
+    </div>
   );
 };
 
--- src/components/Checkout/PlaceOrder.jsx
+++ src/components/Checkout/PlaceOrder.jsx
@@ -1,13 +1,92 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
-const PlaceOrder = () => {
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+
+const PlaceOrder = ({ total }) => {
+  const [error, setError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const stripe = useStripe();
+  const elements = useElements();
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  useEffect(() => {
+    user &&
+      axiosSecure
+        .post("/create-payment-intent", { price: total })
+        .then((res) => {
+          setClientSecret(res.data.clientSecret);
+        });
+  }, [axiosSecure, total, user]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+    if (card == null) {
+      return;
+    }
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    if (error) {
+      setError(error.message);
+    } else {
+      setError("");
+    }
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user.email || "anonymous",
+            name: user.displayName || "anonymous",
+          },
+        },
+      });
+
+    if (confirmError) {
+      console.log("confirm error", confirmError);
+    } else {
+      if (paymentIntent.status === "succeeded") {
+        toast.success("payment success");
+        setTransactionId(paymentIntent.id);
+        console.log(paymentIntent.id);
+      }
+    }
   };
   return (
-    <div>
+    <div className="mt-4">
       <form onSubmit={handleSubmit}>
-        <PaymentElement />
-        <button>Submit</button>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="mt-4 w-full text-base bg-primary text-white p-1 rounded-md disabled:bg-gray-300 disabled:cursor-not-allowed"
+          type="submit"
+          disabled={!stripe || !clientSecret}
+        >
+          Pay Now
+        </button>
+        <p className="text-red-700 text-xs mt-1">{error}</p>
       </form>
     </div>
   );
