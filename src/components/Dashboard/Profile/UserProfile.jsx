d16575d49ebf9e33abfd1b916e6e3c4492047307
Masud Rana <masud040@gmail.com>
2024-01-06 00:17:09 +0600

    implement layout in profile section

--- src/components/Dashboard/Profile/UserProfile.jsx
+++ src/components/Dashboard/Profile/UserProfile.jsx
@@ -1,5 +1,18 @@
+import useGetAddress from "../../../hooks/useGetAddress";
+
 const UserProfile = () => {
-  return <div>eee</div>;
+  const [userAddress, refetch] = useGetAddress();
+  return (
+    <>
+      <h1 className="text-center text-xl text-gray-700">My Profile</h1>
+      <div className="md:grid grid-cols-2 gap-6 space-y-6 md:space-y-0 mt-6">
+        <div className="border min-h-[100px] rounded-md border-gray-300 drop-shadow-xl bg-gray-50"></div>
+
+        <div className="border min-h-[100px] rounded-md  border-gray-300 drop-shadow-xl bg-gray-50"></div>
+      </div>
+      ;
+    </>
+  );
 };
 
 export default UserProfile;
