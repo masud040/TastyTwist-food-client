0853a63bbcbc6d15f1408024ab9e2f9cf43c4aba
masud040 <masudinfo040@gmail.com>
2024-01-12 21:58:06 +0600

    add a nice table for show payment details

---
+++ src/components/Table/HistoryTableRow.jsx
@@ -0,0 +1,15 @@
+export default function HistoryTableRow() {
+  return (
+    <tr className="border-b border-gray-300">
+      <th
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+      >
+        Apple MacBook Pro 17"
+      </th>
+      <td className="px-6 py-4">Silver</td>
+      <td className="px-6 py-4">Laptop</td>
+      <td className="px-6 py-4">$2999</td>
+    </tr>
+  );
+}
--- src/pages/User/PaymentHistory.jsx
+++ src/pages/User/PaymentHistory.jsx
@@ -1,20 +1,37 @@
+import HistoryTableRow from "../../components/Table/HistoryTableRow";
 import useGetOrderItem from "../../hooks/useGetOrderItem";
 
 const PaymentHistory = () => {
   const [orderItems] = useGetOrderItem();
+  console.log(orderItems);
 
   return (
-    <div>
-      <table>
-        <thead>
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left ">
+        <thead className=" text-gray-700  uppercase">
           <tr>
-            <th></th>
-            <th>TransactionId</th>
-            <th>Date</th>
-            <th>Price</th>
+            <th scope="col" className="px-6 py-3">
+              Order Id
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Price
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Transaction Id
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Date
+            </th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {orderItems?.map((order) => (
+            <HistoryTableRow key={order._id} order={order} />
+          ))}
+        </tbody>
       </table>
     </div>
   );
