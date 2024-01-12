3df2b8290ef82c19d9a4a4340e0c295eac7132dc
masud040 <masudinfo962@gmail.com>
2024-01-12 23:02:16 +0600

    complete payment history page

--- src/components/Table/HistoryTableRow.jsx
+++ src/components/Table/HistoryTableRow.jsx
@@ -1,15 +1,22 @@
-export default function HistoryTableRow() {
+export default function HistoryTableRow({ order }) {
+  const { total, orderId, transactionId, date, cartItems } = order || {};
   return (
-    <tr className="border-b border-gray-300">
-      <th
-        scope="row"
-        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-      >
-        Apple MacBook Pro 17"
-      </th>
-      <td className="px-6 py-4">Silver</td>
-      <td className="px-6 py-4">Laptop</td>
-      <td className="px-6 py-4">$2999</td>
+    <tr className="border-b border-gray-300 text-gray-700 text-sm ">
+      <td className="px-3 py-3">#{orderId}</td>
+      <td className="px-3 py-3 ">
+        {cartItems?.map((item, index) => (
+          <ul key={item._id}>
+            <li>
+              {index + 1}. {item.name}
+            </li>
+          </ul>
+        ))}
+      </td>
+      <td className="px-3 py-3">TK{total}</td>
+      <td className="px-3 py-3">{transactionId}</td>
+      <td className="px-3 py-3">
+        {date && date.split(" ").slice(1, 4).join(" ")}
+      </td>
     </tr>
   );
 }
--- src/pages/User/PaymentHistory.jsx
+++ src/pages/User/PaymentHistory.jsx
@@ -5,24 +5,31 @@ const PaymentHistory = () => {
   const [orderItems] = useGetOrderItem();
 
   return (
-    <div className="relative overflow-x-auto">
-      <table className="w-full text-sm text-left ">
-        <thead className=" text-gray-700  uppercase">
-          <tr>
-            <th className="px-6 py-3">Order Id</th>
-            <th className="px-6 py-3">Name</th>
-            <th className="px-6 py-3">Price</th>
-            <th className="px-6 py-3">Transa Id</th>
-            <th className="px-6 py-3">Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orderItems?.map((order) => (
-            <HistoryTableRow key={order._id} order={order} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div>
+        <h1 className="text-center text-gray-800 text-2xl font-semibold mb-8">
+          Payment History
+        </h1>
+      </div>
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-left">
+          <thead className=" text-gray-700 ">
+            <tr>
+              <th className="px-3">Order Id</th>
+              <th className="px-3">Name</th>
+              <th className="px-3">Price</th>
+              <th className="px-3">Transaction Id</th>
+              <th className="px-3">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderItems?.map((order) => (
+              <HistoryTableRow key={order._id} order={order} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
