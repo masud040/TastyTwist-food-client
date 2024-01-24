fc0917921700226a916f1effa4a2269200c1421b
masud040 <masudinfo962@gmail.com>
2024-01-24 15:18:43 +0600

    add a modal for get feedback end of the user

--- src/components/Card/OrderCard.jsx
+++ src/components/Card/OrderCard.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import Swal from "sweetalert2";
+import FeedbackModal from "../Modal/FeebackModal/FeedbackModal";
 
 const OrderCard = ({ item, estimatedDate, status, id }) => {
   const { name, image, count } = item || {};
-
+  const [isFeebackOpen, setIsFeedbackOpen] = useState(false);
+  const [showGreeting, setShowGreeting] = useState(false);
+  const handleSaveFeeback = (event, feedback) => {
+    setShowGreeting(true);
+    event.preventDefault();
+    console.log(feedback);
+  };
   const handleCancel = async (id) => {
     Swal.fire({
       title: "Are you sure?",
@@ -18,30 +26,53 @@ const OrderCard = ({ item, estimatedDate, status, id }) => {
       }
     });
   };
+  const closeModal = () => {
+    setIsFeedbackOpen(false);
+    setShowGreeting(false);
+  };
   return (
-    <div className=" text-gray-800 bg-gray-100 px-2 py-3 rounded-md drop-shadow-lg space-y-2 overflow-x-auto">
-      <div className=" flex gap-6 justify-between  items-center  text-sm">
-        <img
-          src={image}
-          className="w-[72px] md:w-28 h-12 rounded-sm md:h-14 "
-          alt=""
+    <>
+      {isFeebackOpen && (
+        <FeedbackModal
+          isFeebackOpen={isFeebackOpen}
+          onClose={closeModal}
+          onSave={handleSaveFeeback}
+          greeting={showGreeting}
         />
-        <p>{name}</p>
-        <p className="flex items-center gap-2">
-          Qty: <span>{count}</span>
+      )}
+      <div className=" text-gray-800 bg-gray-100 px-2 py-3 rounded-md drop-shadow-lg space-y-2 overflow-x-auto">
+        <div className=" flex gap-6 justify-between  items-center  text-sm">
+          <img
+            src={image}
+            className="w-[72px] md:w-28 h-12 rounded-sm md:h-14 "
+            alt=""
+          />
+          <p>{name}</p>
+          <p className="flex items-center gap-2">
+            Qty: <span>{count}</span>
+          </p>
+          {status !== "delivered" ? (
+            <p className="text-xs bg-primary text-white p-1 w-20 text-center rounded-xl px-2">
+              {status}
+            </p>
+          ) : (
+            <button
+              onClick={() => setIsFeedbackOpen(true)}
+              className="text-xs bg-primary text-white p-1 px-2 text-center rounded-xl"
+            >
+              Share Your Feedback
+            </button>
+          )}
+          <button onClick={() => handleCancel(id)} className="text-primary">
+            Cancel
+          </button>
+        </div>
+        <p className="text-green-500">
+          Estimated Delivery Date
+          {estimatedDate}
         </p>
-        <p className="text-xs bg-gray-300 p-1 w-20 text-center rounded-xl px-2">
-          {status}
-        </p>
-        <button onClick={() => handleCancel(id)} className="text-primary">
-          Cancel
-        </button>
       </div>
-      <p className="text-green-500">
-        Estimated Delivery Date
-        {estimatedDate}
-      </p>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/Modal/FeebackModal/FeedbackModal.jsx
@@ -0,0 +1,110 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import Rating from "react-rating";
+export default function FeedbackModal({
+  isFeebackOpen,
+  onClose,
+  onSave,
+  greeting,
+}) {
+  const [feedback, setFeedback] = useState({ rating: 0, message: "" });
+
+  return (
+    <Transition appear show={isFeebackOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full min-h-[150px] max-w-sm transform overflow-hidden rounded-md bg-white p-6 text-left align-middle shadow-xl transition-all relative ">
+                <div className="absolute top-3 z-30 right-3">
+                  <span onClick={onClose}>
+                    <IoClose className="text-3xl  text-gray-600 hover:text-gray-900" />
+                  </span>
+                </div>
+                <Dialog.Title
+                  as="h3"
+                  className=" mb-1 text-center font-normal leading-6 text-gray-900"
+                >
+                  Share Your Experience
+                </Dialog.Title>
+                <hr />
+                {!greeting && (
+                  <div className="mt-2 w-full">
+                    <form onSubmit={() => onSave(event, feedback)}>
+                      <div className="mt-6 mb-3">
+                        <p className="mb-3 text-sm">Feeback Rating</p>
+                        <Rating
+                          initialRating={feedback.rating}
+                          onChange={(e) =>
+                            setFeedback({
+                              ...feedback,
+                              rating: e,
+                            })
+                          }
+                          fullSymbol={
+                            <FaStar className="text-[#FFB400] text-[18px]" />
+                          }
+                          emptySymbol={
+                            <FaStar className="text-gray-500 text-[18px]" />
+                          }
+                        />
+                      </div>
+                      <label className="text-sm block">Write about Food</label>
+                      <textarea
+                        className="border text-sm text-gray-700 w-full mt-3 focus:outline-0 focus:ring-1 rounded-md ring-primary transition-color focus:shadow-xl duration-300"
+                        name="message"
+                        placeholder="Rate Your Experience"
+                        value={feedback.message}
+                        onChange={(e) =>
+                          setFeedback({
+                            ...feedback,
+                            message: e.target.value,
+                          })
+                        }
+                        rows="3"
+                      ></textarea>
+
+                      <button
+                        type="submit"
+                        className="w-full mt-6 bg-primary p-2 rounded-lg text-white font-semibold"
+                      >
+                        Submit
+                      </button>
+                    </form>
+                  </div>
+                )}
+                {greeting && (
+                  <h1 className="text-center mt-6 font-semibold text-xl text-gray-800">
+                    Thank you for share your experience !
+                  </h1>
+                )}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
