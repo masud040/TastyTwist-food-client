09fb618d0b1ec7b2cb639ca9f84e10fad0b6dc27
masud040 <masudinfo962@gmail.com>
2024-02-02 15:50:12 +0600

    add some restaurant detail on seller porfile section

--- src/components/Dashboard/Profile/SellerProfile/Restaurant.jsx
+++ src/components/Dashboard/Profile/SellerProfile/Restaurant.jsx
@@ -12,5 +12,30 @@ export default function Restaurant({ restaurant }) {
     menu,
     location,
   } = restaurant || {};
-  return <div></div>;
+  return (
+    <div className="flex justify-between items-center gap-8">
+      <div>
+        <p className="text-sm mb-3">Restaurant Image</p>
+        <img src={image_url} alt={name} className="rounded-lg h-[160px]" />
+      </div>
+      <div className="flex-1">
+        <div>
+          <AddLabel label="Name" text={name} name={true} />
+          <AddLabel label="Email" text={email} />
+          <AddLabel label="Location" text={location} />
+          <AddLabel label="Cuisine" text={cuisine} />
+        </div>
+      </div>
+    </div>
+  );
+}
+function AddLabel({ label, text, name }) {
+  return (
+    <>
+      <label className="text-xs block mt-2">{label}</label>
+      <h4 className={`  ${name ? "text-lg text-primary font-bold" : ""}`}>
+        {text}
+      </h4>
+    </>
+  );
 }
