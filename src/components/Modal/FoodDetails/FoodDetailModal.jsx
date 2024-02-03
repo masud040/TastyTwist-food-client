89fc6750ababe154be27df5f8c8c8b6a5076dbf1
masud040 <masudinfo962@gmail.com>
2024-02-03 22:30:24 +0600

    complete get review hook

--- src/components/Modal/FoodDetails/FoodDetailModal.jsx
+++ src/components/Modal/FoodDetails/FoodDetailModal.jsx
@@ -1,5 +1,6 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
+import useGetReviews from "../../../hooks/useGetReviews";
 import CloseModal from "../../Button/CloseModal";
 import HandleItemCount from "../../HandleItemCout/HandleItemCount";
 import ModalTitle from "../../Title/ModalTitle";
@@ -11,7 +12,8 @@ export default function FoodDetailsModal({
   onAddToCart,
 }) {
   const { _id, name, price, description, image_url } = item || {};
-  console.log(_id);
+  const reviews = useGetReviews(_id, "id");
+  console.log(reviews);
   const [totalCount, setTotalCount] = useState(1);
   const handleDecrement = async () => {
     if (totalCount > 1) {
--- src/hooks/useGetReviews.jsx
+++ src/hooks/useGetReviews.jsx
@@ -1,3 +1,16 @@
-export default function useGetReviews() {
-  return <div></div>;
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+
+export default function useGetReviews(queryName, queryKey) {
+  const axiosSecure = useAxiosSecure();
+  const { data: reviews, refetch } = useQuery({
+    queryKey: ["reviews"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(
+        `/feedbacks?${queryKey}=${queryName}`
+      );
+      return data;
+    },
+  });
+  return [reviews, refetch];
 }
